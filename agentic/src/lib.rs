7e4bb1d7712ee2fa86ef42d089e0d472d100bbab
Afsal Thaj <afsal.taj06@gmail.com>
2025-06-24 11:42:49 +0530

    Initial commit on golem agentic

---
+++ agentic/src/lib.rs
@@ -0,0 +1,10 @@
+wit_bindgen::generate!({
+    path: "wit",
+    world: "agentic",
+    generate_all,
+    generate_unused_types: true,
+    additional_derives: [PartialEq, golem_rust::FromValueAndType, golem_rust::IntoValue],
+    pub_export_macro: true,
+});
+
+
